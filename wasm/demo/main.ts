ddc96f5b6e8d09f2b857c21ccc10987c1694619e
Reilly Grant <reillyg@google.com>
2023-03-29 13:59:49 -0700

    Add text boxes to demo

--- wasm/demo/main.ts
+++ wasm/demo/main.ts
@@ -1,17 +1,31 @@
 import { LevelDb } from './leveldb_db.js';
 
-let db = new LevelDb('hello_world');
+const db = new LevelDb('hello_world');
 
-try {
-  await db.put('key', 'value');
-  console.log('I put therefore I am');
-} catch (e) {
-  console.log(e);
-}
+const getButton = document.getElementById('get');
+getButton.addEventListener('click', async () => {
+  const keyInput = document.getElementById('get_key') as HTMLInputElement;
+  const resultSpan = document.getElementById('get_result') as HTMLSpanElement;
 
-try {
-  let value = await db.get('key');
-  console.log('Gotten value should be [value] and is [' + value + ']');
-} catch (e) {
-  console.log(e);
-}
+  try {
+    resultSpan.textContent = await db.get(keyInput.value);
+  } catch (e) {
+    resultSpan.textContent = `${e.name}: ${e.message}`;
+    console.error(e);
+  }
+});
+
+const putButton = document.getElementById('put');
+putButton.addEventListener('click', async () => {
+  const keyInput = document.getElementById('put_key') as HTMLInputElement;
+  const valueInput = document.getElementById('put_value') as HTMLInputElement;
+  const resultSpan = document.getElementById('put_result') as HTMLSpanElement;
+
+  try {
+    await db.put(keyInput.value, valueInput.value);
+    resultSpan.textContent = 'Done';
+  } catch (e) {
+    resultSpan.textContent = `${e.name}: ${e.message}`;
+    console.error(e);
+  }
+});

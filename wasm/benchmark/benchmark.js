f5f6d2b62a0ebd88454cf732769e83d6d81657b8
Evan Stade <estade@chromium.org>
2023-03-30 22:20:37 +0000

    Improve benchmarks and add delete benches

--- wasm/benchmark/benchmark.js
+++ wasm/benchmark/benchmark.js
@@ -1,4 +1,4 @@
-import { get as idbGet, set as idbSet, setMany as idbSetMany, clear as idbClear} from './idbkv/index.js';
+import { get as idbGet, set as idbSet, setMany as idbSetMany, clear as idbClear, del as idbDel} from './idbkv/index.js';
 import { LevelDb } from './leveldb_db.js';
 
 // Default values for benchmark parameters.
@@ -11,6 +11,7 @@ const indexedDb = {
   get: idbGet,
   set: idbSet,
   setMany: idbSetMany,
+  remove: idbDel,
   clear: idbClear,
 }
 
@@ -41,6 +42,10 @@ class LevelDbImpl {
     return this.db_.putMany(pairs);
   }
 
+  remove(key) {
+    return this.db_.remove(key);
+  }
+
   clear() {
     return null;
     // notimpl
@@ -59,8 +64,8 @@ function writeOutput(text) {
   getEm('output-area').textContent += text + '\n';
 }
 
-function generateKey() {
-  return Math.random().toString(16).slice(2);
+function toKey(i) {
+  return i.toString(16).padStart(8, '0');
 }
 
 /* Set up the kv pair db by writing a bunch of data. */
@@ -70,7 +75,7 @@ async function doWrites(startTimer) {
   savedKeys = [];
   for (let i = 0; i < numEntries; ++i) {
     let value = Array.from({length: valueSize / 8}, (i) => Math.floor(Math.random() * maxVal).toString(36)).join('');
-    let key = generateKey();
+    let key = toKey(i);
     savedKeys.push(key);
     pairs.push([key, value]);
   }
@@ -80,57 +85,88 @@ async function doWrites(startTimer) {
 }
 
 const tests = {
-readRandom: async function(startTimer) {
-  let keys = [];
-  let promises = [];
-  for (let i = -1; i < numReads; ++i) {
-    // Select randomly among existing keys.
-    keys.push(savedKeys[Math.floor(Math.random() * savedKeys.length)]);
-  }
-  // Selection of keys is not an interesting thing to test, so don't start the timer until that's done.
-  startTimer();
+  readRandom: async function(startTimer) {
+    let keys = [];
+    let promises = [];
+    for (let i = 0; i < numReads; ++i) {
+      // Select randomly among existing keys.
+      keys.push(savedKeys[Math.floor(Math.random() * savedKeys.length)]);
+    }
+    // Selection of keys is not an interesting thing to test, so don't start the timer until that's done.
+    startTimer();
 
-  keys.forEach((key) => promises.push(activeBackend.get(key)));
-  // These promises should all resolve since the keys do exist.
-  return Promise.all(promises);
-},
+    keys.forEach((key) => promises.push(activeBackend.get(key)));
+    // These promises should all resolve since the keys do exist.
+    return Promise.all(promises);
+  },
 
+  readMissing: async function(startTimer) {
+    let keys = [];
+    let promises = [];
+    for (let i = 0; i < numReads; ++i) {
+      // Make up new keys.
+      keys.push(Math.random().toString());
+    }
+    // Generation of random keys is not an interesting thing to test, so don't start the timer until that's done.
+    startTimer();
 
-readMissing: async function(startTimer) {
-  let keys = [];
-  let promises = [];
-  for (let i = -1; i < numReads; ++i) {
-    // Make up new keys.
-    keys.push(generateKey() + 'g');
-  }
-  // Generation of random keys is not an interesting thing to test, so don't start the timer until that's done.
-  startTimer();
+    keys.forEach((key) => promises.push(activeBackend.get(key)));
+    // These promises should reject (depending on backend) since the keys don't exist.
+    return Promise.allSettled(promises);
+  },
 
-  keys.forEach((key) => promises.push(activeBackend.get(key)));
-  // These promises should reject (depending on backend) since the keys don't exist.
-  return Promise.allSettled(promises);
-},
+  readHot: async function(startTimer) {
+    let keys = [];
+    let promises = [];
+    for (let i = 0; i < numReads; ++i) {
+      // Select randomly among existing keys, but only 1%.
+      keys.push(savedKeys[Math.floor(Math.random() * (savedKeys.length / 100))]);
+    }
+    // Generation of keys is not an interesting thing to test, so don't start the timer until that's done.
+    startTimer();
 
-readHot: async function(startTimer) {
-  let keys = [];
-  let promises = [];
-  for (let i = 0; i < 1; ++i) {
-    // Select randomly among existing keys, but only 1%.
-    keys.push(savedKeys[Math.floor(Math.random() * (savedKeys.length / 100))]);
-  }
-  // Generation of keys is not an interesting thing to test, so don't start the timer until that's done.
-  startTimer();
+    keys.forEach((key) => promises.push(activeBackend.get(key)));
+    // These promises should all resolve since the keys do exist.
+    return Promise.all(promises);
+  },
 
-  keys.forEach((key) => promises.push(activeBackend.get(key)));
-  // These promises should all resolve since the keys do exist.
-  return Promise.all(promises);
-}
+  deleteRandom: async function(startTimer) {
+    let keys = [];
+    let promises = [];
+    for (let i = 0; i < numReads; ++i) {
+      // Select randomly among existing keys.
+      keys.push(savedKeys[Math.floor(Math.random() * savedKeys.length)]);
+    }
+    // Selection of keys is not an interesting thing to test, so don't start the timer until that's done.
+    startTimer();
+
+    keys.forEach((key) => promises.push(activeBackend.remove(key)));
+    // These promises should all resolve since the keys do exist.
+    return Promise.all(promises);
+  },
+
+  deleteSequential: async function(startTimer) {
+    let keys = [];
+    let promises = [];
+    for (let i = 0; i < numReads; ++i) {
+      // Select randomly among existing keys.
+      keys.push(savedKeys[i]);
+    }
+    // Selection of keys is not an interesting thing to test, so don't start the timer until that's done.
+    startTimer();
+
+    keys.forEach((key) => promises.push(activeBackend.remove(key)));
+    // These promises should all resolve since the keys do exist.
+    return Promise.all(promises);
+  },
 }
 
 function updateTestDescriptions() {
   tests.readRandom.description = 'reads ' + numReads + ' random keys that exist in the database';
   tests.readMissing.description = 'reads ' + numReads + ' random keys that are not in the database';
   tests.readHot.description = 'reads 1% of the keys in the database ' + numReads + ' times';
+  tests.deleteRandom.description = 'deletes ' + numReads + ' keys at random';
+  tests.deleteSequential.description = 'deletes ' + numReads + ' keys in order';
 }
 
 async function fillStore() {

b7dfc7217a9010bb65ea73d076a82db57787a45d
Austin Sullivan <asully@chromium.org>
2023-03-30 22:59:54 +0000

    fix typo in copyright

--- wasm/idl/write_options.cc
+++ wasm/idl/write_options.cc
@@ -2,7 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file. See the AUTHORS file for names of contributors.
 
-#include "status.h"
-#include "iterator.h"
-#include "dbwrapper.h"
-#include "glue.cpp"
+#include "write_options.h"
+
+// WriteOptions::WriteOptions(bool sync) {
+//   options_ = { .sync = sync };
+// }
+
+
+WriteOptions::WriteOptions(bool sync): sync_(sync) {}
